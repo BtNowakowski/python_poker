23d470b5ca8db251c2ffe95b8953972fe63891fe
Bartosz <bt_nowakowski@outlook.com>
2023-09-11 12:14:17 +0200

    added variable for computers possible bets

--- app/source/computer.py
+++ app/source/computer.py
@@ -5,6 +5,7 @@ from random import choice as random_choice
 class Computer(Player):
     def __init__(self, poker_game, money=1000):
         super().__init__(poker_game, money)
+        self.possible_bets = [0, 100, 200, 300, 400, 500]
 
     def make_decision(self, player_bet, is_player_passing, is_player_all_in):
         if is_player_all_in:
@@ -27,7 +28,7 @@ class Computer(Player):
         elif is_player_passing or player_bet == self.current_bet:
             random_bet = self.money + 1
             while random_bet > self.money:
-                random_bet = random_choice([0, 100, 200, 300, 400, 500])
+                random_bet = random_choice(self.possible_bets)
             if random_bet == 0:
                 self.pass_q()
             else:
